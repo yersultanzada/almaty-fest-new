a2518b32f45ff67edd7ea5b5a32b9e6d1a87e962
yersultanzada <yersultan.zada@gmail.com>
2018-07-16 18:16:59 +0600

    main page done

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
 $(document).ready(function () {
+    /*Cookies.remove('popupTimer');*/
+
+    /***** Вставляем куки для попапа ****/
+    function hidePopup() {
+        $(".main_popup").css({
+            'display': 'none'
+        });
+        Cookies.set('popupTimer', 'value', {expires: (1 / 3)});
+    }
+    function showPopup() {
+        $(".main_popup").css({
+            'display': 'block'
+        })
+    }
+    $(window).load(function() {
+        var windowWidth = $(window).width();
+        if(windowWidth > 1023 && ! Cookies.get('popupTimer')) {
+            showPopup();
+            setTimeout(hidePopup, 4000);
+        }
+    });
+
+    /***** Активируем карусели ****/
     $('#main-carousel').owlCarousel({
         items: 1,
         loop:true,
@@ -43,4 +66,37 @@ $(document).ready(function () {
             }
         }
     });
+
+    /***** Менюшка на главной со сдвигом вниз ****/
+    $("#dropdownMenuLink").click(function () {
+
+        if($('#dropdownMenuLink').parent().hasClass('_show')){
+            $('#dropdownMenuLink').parent().removeClass('_show');
+        }else{
+            $('#dropdownMenuLink').parent().addClass('_show');
+        }
+
+        $('._dropdown_menu').slideToggle('fast');
+        if($('._dropdown_menu').hasClass('_show')){
+            $('._dropdown_menu').removeClass('_show');
+        }else{
+            $('._dropdown_menu').addClass('_show');
+        }
+    });
+
+    $("._dropdown_item").click(function () {
+        if($(this).hasClass('activated')){
+            $(this).removeClass('activated');
+        }else{
+            $(this).addClass('activated');
+        }
+
+        $(this).next().slideToggle('fast');
+        if($(this).next().hasClass('visible')){
+            $(this).next().removeClass('visible');
+        }else{
+            $(this).next().addClass('visible');
+        }
+    });
+
 });
