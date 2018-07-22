b43b21dd54ff5eff16dc1a13d8f31c5a0460d5c4
yersultanzada <yersultan.zada@gmail.com>
2018-07-22 13:57:44 +0600

    end festival part

--- js/main.js
+++ js/main.js
@@ -49,7 +49,6 @@ $(document).ready(function () {
             "<i class='mdi mdi-menu-left'></i>",
             "<i class='mdi mdi-menu-right'></i>"
         ],
-        responsiveClass:true,
         responsive:{
             0:{
                 items:2,
@@ -63,6 +62,90 @@ $(document).ready(function () {
                 items:5,
                 nav:true,
                 loop:false
+            }
+        }
+    });
+    $('#inner_genPartners').owlCarousel({
+        items: 5,
+        loop:true,
+        margin:10,
+        dots: false,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:false,
+        nav:true,
+        navText: [
+            "<i class='mdi mdi-menu-left'></i>",
+            "<i class='mdi mdi-menu-right'></i>"
+        ],
+        responsive:{
+            0:{
+                items:2,
+                nav:true
+            },
+            600:{
+                items:3,
+                nav:false
+            },
+            1000:{
+                items:5,
+                nav:true
+            }
+        }
+    });
+    $('#inner_sponsors').owlCarousel({
+        items: 4,
+        loop:true,
+        margin:10,
+        dots: false,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:false,
+        nav:true,
+        navText: [
+            "<i class='mdi mdi-menu-left'></i>",
+            "<i class='mdi mdi-menu-right'></i>"
+        ],
+        responsive:{
+            0:{
+                items:2,
+                nav:true
+            },
+            600:{
+                items:3,
+                nav:false
+            },
+            1000:{
+                items:4,
+                nav:true
+            }
+        }
+    });
+    $('#inner_partners').owlCarousel({
+        items: 3,
+        loop:true,
+        margin:10,
+        dots: false,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:false,
+        nav:true,
+        navText: [
+            "<i class='mdi mdi-menu-left'></i>",
+            "<i class='mdi mdi-menu-right'></i>"
+        ],
+        responsive:{
+            0:{
+                items:2,
+                nav:true
+            },
+            600:{
+                items:3,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:true
             }
         }
     });
